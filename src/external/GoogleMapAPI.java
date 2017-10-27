19cb82b7a729bfb8c988a1e949c2ecfeab81b8a1
n6134123 <n6134123@gmail.com>
2017-10-26 20:35:56 -0700

    First commit.

---
+++ src/external/GoogleMapAPI.java
@@ -0,0 +1,15 @@
+package external;
+
+import java.util.List;
+
+import entity.Item;
+
+public class GoogleMapAPI implements ExternalAPI{
+
+	@Override
+	public List<Item> search(double lat, double lon, String term) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
